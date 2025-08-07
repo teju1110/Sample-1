b37cc4fccdaa032d03cf51e017fe69c65b79b99c
teju1110 <tejaswinij@vananir.com>
2025-08-07 09:25:20 +0530

    Add files via upload

--- data.js
+++ data.js
@@ -20,11 +20,56 @@ var APP_DATA = {
       ],
       "faceSize": 1024,
       "initialViewParameters": {
+        "yaw": 1.5568575313690536,
+        "pitch": 0,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0776531183833562,
+          "pitch": 0.01213489662445788,
+          "rotation": 0,
+          "target": "0-panorama"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-1panorama",
+      "name": "1Panorama",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -2.3980665191469424,
+          "pitch": -0.04135672336171936,
+          "rotation": 0,
+          "target": "1-1panorama"
+        }
+      ],
       "infoHotspots": []
     }
   ],
